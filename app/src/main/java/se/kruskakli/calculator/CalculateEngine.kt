cafb48db1555e6d6cd142f86c7f6840c4965881b
Torbjorn Tornkvist <ttornkvi@cisco.com>
2024-01-14 01:10:15 +0100

    Replacing a number last entered if the PI button is pressed which replaces that number

--- app/src/main/java/se/kruskakli/calculator/CalculateEngine.kt
+++ app/src/main/java/se/kruskakli/calculator/CalculateEngine.kt
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) [2024] [Torbjörn Törnkvist - kruskakli@gmail.com]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package se.kruskakli.calculator
 
 import androidx.compose.runtime.getValue
@@ -241,6 +264,15 @@ class CalculateEngine {
 
     private fun enterPi() {
         val pi = 3.1416
+
+        // If the last item in displayItems is a number, remove it
+        if (displayItems.isNotEmpty() && displayItems.last().toDoubleOrNull() != null) {
+            displayItems.removeAt(displayItems.size - 1)
+            if (numbers.isNotEmpty()) {
+                numbers.pop()
+            }
+        }
+        
         expression = ""
         numbers.push(pi)
         displayItems.add(pi.toString())
